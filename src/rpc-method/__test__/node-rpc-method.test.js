06f36e3e6eb5763df894a814adab497bfadc35ca
Xiangzhou Xia <xiaxiangzhou@gmail.com>
2019-02-26 03:08:53 -0800

    add send action

--- src/rpc-method/__test__/node-rpc-method.test.js
+++ src/rpc-method/__test__/node-rpc-method.test.js
@@ -37,7 +37,7 @@ test('RpcMethod.getBlockMetas', async t => {
   t.deepEqual(resp1.blkMetas[0], resp4.blkMetas[0]);
 });
 
-test.skip('RpcMethod.getActionsByIndex', async t => {
+test('RpcMethod.getActionsByIndex', async t => {
   const client = new RpcMethod('35.192.119.63:31500');
   // test getActionsByIndex
   const resp1 = await client.getActions({byIndex: {start: 10, count: 1}});
@@ -48,7 +48,7 @@ test.skip('RpcMethod.getActionsByIndex', async t => {
   t.deepEqual(resp3.actions.length, 0);
 });
 
-test.skip('RpcMethod.getActionsByAddress', async t => {
+test('RpcMethod.getActionsByAddress', async t => {
   const client = new RpcMethod('35.192.119.63:31500');
   // test getActionByAddress
   const resp5 = await client.getActions({byAddr: {address: 'io1hc6ndjzm3frn5e7a83qhm7m3a9gxsyg9teg9j8', start: 0, count: 1}});
@@ -59,7 +59,7 @@ test.skip('RpcMethod.getUnconfirmedActionsByAddress', async t => {
   const client = new RpcMethod('35.192.119.63:31500');
   // test getUnconfirmedActionsByAddress
   const resp6 = await client.getActions({unconfirmedByAddr: {address: 'io1hc6ndjzm3frn5e7a83qhm7m3a9gxsyg9teg9j8', start: 0, count: 1}});
-  t.deepEqual(resp6.actions.length, 0);
+  t.deepEqual(resp6.actions.length, 1);
 });
 
 test.skip('RpcMethod.getActionsByHash', async t => {
@@ -69,7 +69,7 @@ test.skip('RpcMethod.getActionsByHash', async t => {
   t.deepEqual(resp4.actions.length, 0);
 });
 
-test.skip('RpcMethod.getActionsByBlock', async t => {
+test('RpcMethod.getActionsByBlock', async t => {
   const client = new RpcMethod('35.192.119.63:31500');
   // test getActionsByBlock
   const blks = await client.getBlockMetas({byIndex: {start: 10, count: 1}});
@@ -88,4 +88,47 @@ test.skip('RpcMethod.getReceiptByAction', async t => {
   const client = new RpcMethod('35.192.119.63:31500');
   const resp = await client.getReceiptByAction({actionHash: '01d5c895f3b066e695d516884bec9977404875aeb15216bc087dbc0a1ef9aed1'});
   t.deepEqual(resp.receipt, 1);
+});
+
+test.skip('RpcMethod.sendActionTransfer', async t => {
+  const client = new RpcMethod('35.192.119.63:31500');
+  const blks = await client.getBlockMetas({byIndex: {start: 10, count: 1}});
+  t.deepEqual(blks.blkMetas.length, 1);
+  const resp1 = await client.getActions({byBlk: {blkHash: blks.blkMetas[0].hash, start: 0, count: 15}});
+
+  let transfer = null;
+  for (let index = 0; index < resp1.actions.length; index++) {
+    if (resp1.actions[index].core.transfer) {
+      transfer = resp1.actions[index];
+      break;
+    }
+  }
+
+  if (transfer) {
+    const accountOld = await client.getAccount({
+      address: transfer.core.transfer.recipient,
+    });
+    transfer.core.nonce++;
+    await client.sendAction({action: transfer});
+    const accountNew = await client.getAccount({
+      address: transfer.core.transfer.recipient,
+    });
+    t.notDeepEqual(accountNew.accountMeta.balance, accountOld.accountMeta.balance);
+  }
+});
+
+test('RpcMethod.sendAction', async t => {
+  const client = new RpcMethod('35.192.119.63:31500');
+  const blks = await client.getBlockMetas({byIndex: {start: 10, count: 1}});
+  t.deepEqual(blks.blkMetas.length, 1);
+  const resp1 = await client.getActions({byBlk: {blkHash: blks.blkMetas[0].hash, start: 0, count: 15}});
+
+  for (let index = 0; index < resp1.actions.length; index++) {
+    if (resp1.actions[index].core.execution) {
+      await client.sendAction({action: resp1.actions[index]});
+    }
+    if (resp1.actions[index].core.vote) {
+      await client.sendAction({action: resp1.actions[index]});
+    }
+  }
 });
--- src/rpc-method/types.js
+++ src/rpc-method/types.js
@@ -326,7 +326,7 @@ export interface IAction {
   core?: IActionCore | null,
 
   /** Action senderPubkey */
-  senderPubkey?: Uint8Array | null,
+  senderPubKey?: Uint8Array | null,
 
   /** Action signature */
   signature?: Uint8Array | null,
@@ -414,3 +414,12 @@ export interface IGetReceiptByActionResponse {
   /** GetReceiptByActionResponse receipt */
   receipt?: IReceipt | null,
 }
+
+/** Properties of a SendActionRequest. */
+export interface ISendActionRequest {
+  /** SendActionRequest action */
+  action?: IAction | null,
+}
+
+/** Properties of a SendActionResponse. */
+export interface ISendActionResponse {}
