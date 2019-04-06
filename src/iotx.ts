c535f2e224a733ed4ef9868611aa95431ba49acc
Tian Pan <puncsky@gmail.com>
2019-04-06 16:20:33 -0700

    add accounts to iotx

--- src/iotx.ts
+++ src/iotx.ts
@@ -1,7 +1,10 @@
+import { Accounts } from "./account/accounts";
 import RpcMethod from "./rpc-method";
 
 export class Iotx extends RpcMethod {
+  public accounts: Accounts;
   constructor(hostname: string) {
     super(hostname);
+    this.accounts = new Accounts(this);
   }
 }
