03851d5d3b4082c7ae4c32881a25bec3b9d99d27
Tian Pan <puncsky@gmail.com>
2019-07-03 16:27:18 -0700

    add wss plugin (#270)

--- src/action/__test__/action.test.ts
+++ src/action/__test__/action.test.ts
@@ -13,3 +13,18 @@ test("TestAction_SerializationDeserialization", async t => {
     "0a033435361229696f313837777a703038766e686a6a706b79646e723937716c68386b683064706b6b797466616d386a1a0c68656c6c6f20776f726c6421"
   );
 });
+
+test("TestAction_ActionCoreDeserialization", async t => {
+  const core = new actionPb.ActionCore();
+  core.setVersion(1);
+  const tf = new actionPb.Transfer();
+  tf.setAmount("666");
+  core.setTransfer(tf);
+  t.deepEqual(
+    // @ts-ignore
+    actionPb.ActionCore.deserializeBinary(core.serializeBinary())
+      .getTransfer()
+      .getAmount(),
+    "666"
+  );
+});
---
+++ src/plugin/__test__/ws.test.ts
@@ -0,0 +1,58 @@
+import test from "ava";
+import dotenv from "dotenv";
+import sleepPromise from "sleep-promise";
+import Antenna from "../../antenna";
+import { WsSignerPlugin } from "../ws";
+import { toRau } from "../../account/utils";
+
+dotenv.config();
+
+test.skip("WsSignerPlugin", async t => {
+  await Promise.all(
+    [
+      {
+        privateKey: "redacted",
+        address: "redacted",
+        plugin: undefined
+      },
+      {
+        privateKey: "",
+        address: "",
+        plugin: new WsSignerPlugin("wss://localhost:64102/")
+      }
+    ].map(async tt => {
+      const antenna = new Antenna(String(process.env.IOTEX_CORE), {
+        signer: tt.plugin
+      });
+      if (tt.privateKey) {
+        antenna.iotx.accounts.privateKeyToAccount(tt.privateKey);
+      }
+
+      await sleepPromise(200);
+
+      let resp = await antenna.iotx.sendTransfer({
+        to: "io1dw36kqa7mxtjm9xpfrl9ah8drznq67lshfkwc5",
+        from: tt.address,
+        value: toRau("1", "Iotx"),
+        gasLimit: "100000",
+        gasPrice: toRau("1", "Qev")
+      });
+      t.deepEqual(resp.length, 64);
+
+      resp = await antenna.iotx.executeContract(
+        {
+          contractAddress: "io1jmq0epcswzu7vyquxlr9j9jvplwpvtc4d50ze9",
+          amount: "0",
+          abi:
+            '[{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]',
+          method: "get",
+          gasLimit: "100000",
+          gasPrice: "1000000000000000000",
+          from: tt.address
+        },
+        666
+      );
+      t.deepEqual(resp.length, 64);
+    })
+  );
+});
