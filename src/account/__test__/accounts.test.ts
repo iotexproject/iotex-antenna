66a76644593803480b3178bdd300339fc570f0ba
ququzone <xueping.yang@gmail.com>
2019-07-25 08:28:01 +0800

    change accounts access to array access style

--- src/account/__test__/accounts.test.ts
+++ src/account/__test__/accounts.test.ts
@@ -18,3 +18,18 @@ test("Accounts privateToAccount", async t => {
   t.deepEqual(act.privateKey, TEST_ACCOUNT.privateKey);
   t.deepEqual(act.publicKey, TEST_ACCOUNT.publicKey);
 });
+
+test("Accounts array", async t => {
+  const acts = new Accounts(new RpcMethod(""));
+  const act = acts.privateKeyToAccount(TEST_ACCOUNT.privateKey);
+  // @ts-ignore
+  t.deepEqual(1, acts.length);
+  t.deepEqual(act.address, acts[0].address);
+  t.deepEqual(act.privateKey, acts[0].privateKey);
+  t.deepEqual(act.publicKey, acts[0].publicKey);
+
+  const qacc = acts.getAccount(act.address);
+  t.deepEqual(act.address, qacc.address);
+  t.deepEqual(act.privateKey, qacc.privateKey);
+  t.deepEqual(act.publicKey, qacc.publicKey);
+});
