644508c8a9a31313b96a0582acc00f24c78a85d8
ququzone <xueping.yang@gmail.com>
2019-07-26 08:12:07 +0800

    add string type to ABIDefinition

--- src/contract/abi.ts
+++ src/contract/abi.ts
@@ -6,12 +6,12 @@ export interface EthAbiDecodeParametersType {
 export interface ABIDefinition {
   constant?: boolean;
   payable?: boolean;
-  stateMutability?: "pure" | "view" | "nonpayable" | "payable";
+  stateMutability?: "pure" | "view" | "nonpayable" | "payable" | string;
   anonymous?: boolean;
   inputs?: Array<{ name: string; type: ABIDataTypes; indexed?: boolean }>;
   name?: string;
   outputs?: Array<{ name: string; type: ABIDataTypes }>;
-  type: "function" | "constructor" | "event" | "fallback";
+  type: "function" | "constructor" | "event" | "fallback" | string;
 }
 
 export type ABIDataTypes = "uint256" | "boolean" | "string" | "bytes" | string;
