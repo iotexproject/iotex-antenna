9ebba3eeb609fbd7524a5e1701d0ffa71552ba1b
koseoyoung <dorothy0602@gmail.com>
2019-08-20 13:04:09 -0700

    remove log

--- src/rpc-method/__test__/types.test.ts
+++ src/rpc-method/__test__/types.test.ts
@@ -4,7 +4,6 @@ import { fromPbReceipt, ReceiptStatus } from "../types";
 
 test.serial("fromPbReceipt test", async t => {
   const pbReceipt = new actionPb.Receipt();
-  console.log(pbReceipt);
   pbReceipt.setStatus(ReceiptStatus.ErrOutOfGas);
   const receipt = await fromPbReceipt(pbReceipt);
   t.deepEqual(ReceiptStatus.ErrOutOfGas, receipt.status);
